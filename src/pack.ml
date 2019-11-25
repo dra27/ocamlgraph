6b67a5df322526121685f057b67fbe01025a758f
David Allsopp <david.allsopp@metastack.com>
2019-11-25 08:47:43 +0000

    Dune port

--- src/pack.ml
+++ src/pack.ml
@@ -49,7 +49,7 @@ struct
     let zero = 0
     let add = (+)
     let sub = (-)
-    let compare : t -> t -> int = Pervasives.compare
+    let compare : t -> t -> int = Stdlib.compare
   end
 
   include Path.Dijkstra(G)(W)
@@ -66,7 +66,7 @@ struct
     let flow _ = 0
     let add = (+)
     let sub = (-)
-    let compare : t -> t -> int = Pervasives.compare
+    let compare : t -> t -> int = Stdlib.compare
     let zero = 0
   end
 
@@ -94,7 +94,7 @@ struct
 
   module Int = struct
     type t = int
-    let compare : t -> t -> int = Pervasives.compare
+    let compare : t -> t -> int = Stdlib.compare
   end
 
   include Kruskal.Make(G)(Int)
@@ -197,7 +197,7 @@ end
 
 module I = struct
   type t = int
-  let compare : t -> t -> int = Pervasives.compare
+  let compare : t -> t -> int = Stdlib.compare
   let default = 0
 end
 
